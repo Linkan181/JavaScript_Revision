6c86b316049e9f4d417b7b1d383ab61479d46f48
Linkan181 <link4web.pr2004@gmail.com>
2024-03-13 17:46:40 +0530

    revise datatypes

---
+++ 01_datatype.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+/*
+// Primitive DataTypes in JavaScripts 
+1. Number -> 21, 44, 22, 1342422
+2. bigint -> for large number
+3. String 
+4. Boolean -> true/false
+5. null---------> standalone value (special datatype)
+6. undefinded-----> special datatype
+7. symbols  -> unique
+*/
+
+console.log(typeof null); //Object
+console.log(typeof undefined) //undefined
+
+
+// Non-primitiv Data types in Javascript 
+/*
+1. Array
+2. Object
+*/
