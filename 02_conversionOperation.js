d27c565d0fde2a869bb5b9b519e3f6ddf02e0a8a
Linkan181 <link4web.pr2004@gmail.com>
2024-03-13 18:43:13 +0530

    modify conversionOperation

--- 02_conversionOperation.js
+++ 02_conversionOperation.js
@@ -13,5 +13,22 @@ let name="abc"
 let value=undefined;
 //let result = Boolean(value);    //Boolean(null)-->false , Boolean(undefined)-->false, Boolean("")-->false
 let result = Number(value);       //Number(null)-->0 , Number(undefined)-->NaN
-console.log(typeof result)
-console.log(result); 
+// console.log(typeof result)
+// console.log(result); 
+
+// ==============================================================
+
+// console.log("1"+(2+2+5)); //we should use paranthesis for high readability
+// console.log(1+2+"3");
+
+// console.log(((1+2)*4)/2);
+
+let x=4;
+let y=x++; //post-increment
+console.log(x); //5
+console.log(y); //4
+
+let z=++x;
+console.log(z); //6
+
+
